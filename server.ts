a97199539fc4614c826b4e0e7d405bc15b42d88b
ej2brown <ej2brown@edu.uwaterloo.ca>
2020-05-28 18:34:18 -0400

    add db config

--- server.ts
+++ server.ts
@@ -4,6 +4,31 @@ import {GroupExpressRoutes} from './routes/group';
 import {UserExpressRoutes} from './routes/user';
 import {RateExpressRoutes} from './routes/rate';
 
+require("dotenv").config()
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const http = require("http")
+
+app.use(bodyParser.json())
+app.use(cors())
+
+const PORT = 3001
+
+const server = http.createServer(app)
+
+const { Pool } = require("pg")
+const dbParams = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+}
+const db = new Pool(dbParams)
+
+
 const indexRoutes: IRoutes = new IndexExpressRoutes('/', db);
 const groupRoutes: IRoutes = new GroupExpressRoutes('/group', db);
 const userRoutes: IRoutes = new UserExpressRoutes('/user', db);
