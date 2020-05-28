4764f3fed56f47cabaff621d5ab4459863cbdd20
ej2brown <ej2brown@edu.uwaterloo.ca>
2020-05-28 17:31:42 -0400

    add user route

---
+++ routes/user.ts
@@ -0,0 +1,29 @@
+import { AbstractExpressRoutes } from "./expressRoutes";
+const dbHelpersClass = require("./dbHelpers/dbHelpers.ts");
+
+export class UserExpressRoutes extends AbstractExpressRoutes {
+  private m_dbHelpers: any;
+
+  constructor(baseEndpoint: string, db: any) {
+    super(baseEndpoint);
+    this.m_dbHelpers = dbHelpersClass(db);
+    this.setupRouter();
+  }
+
+  private get dbHelpers(): any {
+    return this.m_dbHelpers;
+  }
+
+  // get all groups ids and names the user belongs to. returns data: {array<[id:interger, name:string]>}
+  private setupRouter(): void {
+    this.router.get("/:user_id", (req, res) => {
+      const currentUserId = req.params.user_id;
+      this.dbHelpers
+        .getGroupsNames(currentUserId)
+        .then((data) => {
+          res.send(data);
+        })
+        .catch((e) => e.stack);
+    });
+  }
+}
