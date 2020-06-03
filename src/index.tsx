468ba4f6558efcb9354550931adca8e5202b8e15
ej2brown <ej2brown@edu.uwaterloo.ca>
2020-06-03 15:15:31 -0400

    add react hot loader

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
+
 import App from './App';
+import { AppContainer } from 'react-hot-loader';
+
 import { store } from './store/store';
 import { Provider } from 'react-redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const render = (App) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <AppContainer>
+          <App />
+        </AppContainer>
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+};
+
+render(App);
+
+
+if (module.hot) {
+  module.hot.accept('./App', () => { render(App); });
+}
