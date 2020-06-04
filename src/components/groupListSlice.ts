4c69a42e5e77cb0788886deb754e4489ba53552b
ej2brown <ej2brown@edu.uwaterloo.ca>
2020-06-04 16:47:53 -0400

    fix reducer

--- src/components/groupListSlice.ts
+++ src/components/groupListSlice.ts
@@ -4,24 +4,32 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 interface GroupListState {
     groups: any;
     group: any;
+    posts: {}
 }
 const initialState: GroupListState = {
     groups: [],
     group: {},
+    posts: {}
 };
 
 export const GroupListSlice = createSlice({
     name: "groupList",
     initialState,
     reducers: {
-        //     setGroup: (state, action: PayloadAction<number>) => {
-        //         const { id, groupId } = action.payload
-
-        //         ({ ...state, group: groupId, posts: action.payload })
-        //     },
-        //     retrieveGroups: (state, action: PayloadAction<number>) => {
-        //         state.value += action.payload;
-        // }
+        setGroup: (state, action: PayloadAction<any>) => {
+            // const { id, groupId } = action.payload
+            state.group = action.payload 
+            state.posts = action.payload 
+                // ({ ...state, group: groupId, posts: action.payload })
+        },
+        setGroups: (state, action: PayloadAction<any>) => {
+            state.groups = action.payload;
+        }
     },
 });
 exports.GroupListSlice = GroupListSlice;
+
+
+export const { setGroup, setGroups } = GroupListSlice.actions;
+
+export default GroupListSlice.reducer;
