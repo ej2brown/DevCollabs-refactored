06fa16aaa4e42121dc08d62bc9970651b2aa66f7
ej2brown <ej2brown@edu.uwaterloo.ca>
2020-06-04 16:47:22 -0400

    add group list component

--- src/components/GroupList.tsx
+++ src/components/GroupList.tsx
@@ -1,38 +1,24 @@
-// import React, { useState } from "react";
-// import axios from "axios"
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setGroup, setGroups } from "./groupListSlice"
+import GroupListItem from "./GroupListItem"
 
-export function GroupList() {
-//     const dispatch = useDispatch();
-
-//     const setGroup = (groupId: number) => {
-//         axios
-//           .get(`https://localhost:3001/${groupId}`)
-//           .then(response => {
-//             dispatch(setGroup({ ...state, group: groupId, posts: response.data })
-//           })
-//           .catch(error => error.stack)
-//       }
-
-
-//       const fetchGroups = () => {
-//         axios
-//           .get(`https://dev-collabs-backend.herokuapp.com/group/u/${userId}`)
-//           .then(response => {
-//             axios
-//             .get(`https://dev-collabs-backend.herokuapp.com/group/g/${response.data[0].id}`)
-//             .then(data => {
-//               setState({ ...state,
-//                  posts: data.data,
-//                  group: response.data[0].id,
-//                  groups: response.data,
-//               })
-//             })
-//             .catch(error => error.stack)
-//           })
-//           .catch(error => error.stack)
-//       }
-    
-
-//   return <div>test</div>;
+interface Props {
+    groups: any
+    group: any
+    setGroup: any
 }
-exports.GroupList = GroupList;
+
+export default function GroupList({ groups, setGroup }: Props) {
+    const GroupList = groups.map((group: any) => {
+        return (
+            <GroupListItem
+                key={group.id}
+                id={group.id}
+                name={group.name}
+                setGroup={setGroup}
+            />
+        )
+    })
+    return <div>{GroupList}</div>
+}
---
+++ src/components/GroupListItem.tsx
@@ -0,0 +1,40 @@
+import React from "react"
+import { Button, makeStyles } from "@material-ui/core"
+
+interface Props {
+  setGroup: any
+  name: string
+  id: any
+};
+
+const useStyles = makeStyles(() => ({
+  groupButton: {
+    borderColor: "#551A8B",
+    marginBottom: "0.5em",
+    fontWeight: "bold",
+    color: "#551A8B",
+    backgroundColor: '#FFF',
+    "&:hover": {
+      backgroundColor: "#C0C0C0",
+      color: "#FFF",
+      borderColor: "#551A8B",
+      fontWeight: "bold",
+      borderWidth: '2px',
+    },
+  },
+}));
+
+export default function GroupListItem({ id, name, setGroup } :Props) {
+  const classes = useStyles();
+
+  return (
+    <Button 
+      fullWidth 
+      className={classes.groupButton}
+      variant="outlined" 
+      onClick={() => setGroup(id)}
+    >
+      {name}
+    </Button>
+  )
+};
