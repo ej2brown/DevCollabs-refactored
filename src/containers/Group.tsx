e00bcd23dbf3b88a29034c080e254729b8220925
Liz <ej2brown@uwaterloo.ca>
2020-06-12 16:12:23 -0400

    add group container

---
+++ src/containers/Group.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+//components
+import UserList from "../components/UserList";
+import PostList from "../components/PostList";
+
+const users = [
+    { id: 1, username: "tomas-wen", email: "tomaswen@gmail.com", avatar_image: "/src/images/johnDoe.png" },
+    { id: 2, username: "bradley-mark", email: "bmark88@gmail.com", avatar_image: "/src/images/johnDoe.png" },
+    { id: 3, username: "elizabeth-brown", email: "elizabethjbrown78@gmail.com", avatar_image: "/src/images/johnDoe.png" },
+    { id: 4, username: "aliceLand1", email: "alice@hotmail.com", avatar_image: "/src/images/profile-hex.png" },
+    { id: 5, username: "johnDoe2", email: "john@hotmail.com", avatar_image: "/src/images/johnDoe.png" },
+    { id: 6, username: "alex3", email: "alex@hotmail.com", avatar_image: "/src/images/profile-hex.png" }
+]
+
+const posts = [
+    { id: 10, group_id: 2, user_id: 10, data: "www.google.ca", image_url: "https://www.google.com/logos/doodles/2020/celebrating-mbira-5807476258635776-2xa.gif", username: "elizabeth-brown", created_at: "2020-05-22T01:22:39.224Z" },
+    { id: 5, group_id: 2, user_id: 1, data: "TypeScript is the way to go", image_url: null, username: "tomas-wen", created_at: "2020-05-22T00:03:20.001Z" },
+    { id: 2, group_id: 2, user_id: 3, data: "Hacking for life", image_url: "https://image.shutterstock.com/image-vector/banner…thon-design-sprintlike-event-260nw-1418226719.jpg", username: "elizabeth-brown", created_at: "2020-05-22T00:03:20.001Z" }
+]
+
+const Home = () => (
+    <>
+        <header>
+            <h2>Group Page</h2>
+        </header>
+
+        <section>
+            <UserList users={users} />
+            <PostList posts={posts} />
+        </section>
+    </>
+);
+
+export default Home;
