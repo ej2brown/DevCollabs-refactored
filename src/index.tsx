ef57cd9e47ca624b55d538ad9b6e89df33efbf88
Liz <ej2brown@uwaterloo.ca>
2020-06-13 15:46:52 -0400

    remove redux

--- src/index.tsx
+++ src/index.tsx
@@ -4,27 +4,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 import App from "./App";
-import { AppContainer } from "react-hot-loader";
 
-import { store } from "./store/store";
 import { Provider } from "react-redux";
 
 const render = (App) => {
   ReactDOM.render(
     <React.StrictMode>
-      <Provider store={store}>
-        <AppContainer>
-          <App />
-        </AppContainer>
-      </Provider>
+      <App />
     </React.StrictMode>,
     document.getElementById("root")
   );
 };
 
 render(App);
-
-
-if (module.hot) {
-  module.hot.accept("./App", () => { render(App); });
-}
